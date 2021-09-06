4d4d76273478d8b25b3295322ba3c643a4023387
Gthalles <thalles@gempe.dev>
2021-09-06 15:52:43 -0300

    Preparando o projeto para crescer - Criando um modulo de roteamento para organizar e internalizar as funcionalidades referentes ao curso

--- src/app/cursos/cursos.module.ts
+++ src/app/cursos/cursos.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
 
 import { CursosComponent } from './cursos.component';
 import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 import { CursoNotFoundComponent } from './curso-not-found/curso-not-found.component';
 import { CursosService } from './cursos.service';
+import { CursosRoutingModule } from './cursos.routing.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule
+    CursosRoutingModule
   ],
   declarations: [
     CursosComponent,
