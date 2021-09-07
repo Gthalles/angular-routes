beed50d2e44eb918b4544820c904eaa2779b2e7a
Gthalles <thalles@gempe.dev>
2021-09-07 17:11:30 -0300

    Criando uma nova rota principal para alunos com sub-rotas de cadastro, ediçao e detalhe organizadas em modulo

---
+++ src/app/alunos/alunos.routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from '@angular/router';
+
+import { AlunosComponent } from './alunos.component';
+import { AlunosFormComponent } from './alunos-form/alunos-form.component';
+import { AlunoDetalheComponent } from './aluno-detalhe/aluno-detalhe.component';
+
+const alunosRoutes: Routes = [
+    {path: 'alunos', component: AlunosComponent, children: [
+        {path: 'new', component: AlunosFormComponent},
+        {path: ':id', component: AlunoDetalheComponent},
+        {path: ':id/edit', component: AlunosFormComponent}
+    ]}
+];
+
+@NgModule({
+    imports: [
+        RouterModule.forChild(alunosRoutes)
+    ],
+    exports: [
+        RouterModule
+     ]
+})
+
+
+export class AlunosRoutingModule {
+     
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { AppRoutingModule } from './app.routing.module';
 import { CursosModule } from './cursos/cursos.module';
+import { AlunosModule } from './alunos/alunos.module';
+import { AlunosRoutingModule } from './alunos/alunos.routing.module';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { CursosModule } from './cursos/cursos.module';
   imports: [
     BrowserModule,
     CursosModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AlunosRoutingModule,
+    AlunosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
